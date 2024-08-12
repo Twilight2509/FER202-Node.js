3bc883b8861ba20cbd9bcd54c7ff183979f0adb0
Twilight2509 <onlynanh@gmail.com>
2024-08-12 09:29:40 +0700

    my-app-2
    Copy from my-app

--- Summer2024Bl5/my-app-2/src/App.js
+++ Summer2024Bl5/my-app-2/src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+//ES6
+//Khai bao va khoi tao bien
+let a = 5;
+let b = 6;
+function Cong(a, b) {
+  console.log("a + b = " + (a + b));
+}
+
+//Goi ham
+Cong(a, b);
+
+//Tao 1 list operator
+let mylist=[
+  {a:1,b:2,op:"Cong",result:3},
+  {a:1,b:2,op:"Tru",result:-1},
+  {a:1,b:2,op:"Nhan",result:2},
+  {a:1,b:2,op:"Chia",result:0.5},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className="col-5">
+        <div>
+          <label htmlFor="" style={{ color: "red", fontWeight: "bold" }}>
+            Enter a: <input className="form-control" type="text" id="a" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="" style={{ color: "red", fontWeight: "bold" }}>
+            Enter b: <input className="form-control" type="text" id="b" />
+          </label>
+        </div>
+        <div>
+          <div>
+            <label htmlFor="" style={{ color: "red", fontWeight: "bold" }} id="op">
+              Operator: 
+            </label>
+          </div>
+          <select className="form-select">
+            <option>+</option>
+            <option>-</option>
+            <option>*</option>
+            <option>/</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="" style={{ color: "red", fontWeight: "bold" }}>
+            Result: <input className="form-control" type="text" id="result" />
+          </label>
+        </div>
+        <div>
+          <input
+          type="button" 
+          className="btn btn-outline-success"
+          value="Execute"
+          />
+        </div>
+
+      </div>
+      <div className="col-7">
+        <div>
+            <label htmlFor="" style={{ color: "blue", fontWeight: "bold" }}>
+              List of Operator:
+            </label>
+        </div>
+        <table className="table table-primary table-hover">
+          <thead>
+            <tr>
+              <th>a</th>
+              <th>b</th>
+              <th>Operator</th>
+              <th>Result</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mylist.map(i=>(
+              <tr className="table-secondary">
+                <td>{i.a}</td>
+                <td>{i.b}</td>
+                <td>{i.op}</td>
+                <td>{i.result}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
     </div>
   );
 }
