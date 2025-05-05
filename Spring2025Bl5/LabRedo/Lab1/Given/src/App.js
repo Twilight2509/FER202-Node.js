d7e12255166b543a85037725b89c32432a62c8dd
Twilight2509 <onlynanh@gmail.com>
2025-05-05 23:46:02 +0700

    Update logic

--- Spring2025Bl5/LabRedo/Lab1/Given/src/App.js
+++ Spring2025Bl5/LabRedo/Lab1/Given/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MovieProvider from "./context/MovieContext";
 import HomePage from "./pages/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
     return (
